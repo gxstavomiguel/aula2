af452028baad8f55a49b5f3f3e5c466e6c44618b
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2025-01-11 19:42:22 -0300

    add+

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './components/layout/login/login.component';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { CarroslistComponent } from './components/carros/carroslist/carroslist.component';
 import { CarrosdetailsComponent } from './components/carros/carrosdetails/carrosdetails.component';
+import { MarcaslistComponent } from './components/marca/marcaslist/marcaslist.component';
+import { MarcasdetailsComponent } from './components/marca/marcasdetails/marcasdetails.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: 'full'},
@@ -10,6 +12,9 @@ export const routes: Routes = [
     {path: "admin", component: PrincipalComponent, children: [
         {path: "carros", component: CarroslistComponent},
         {path: "carros/new", component: CarrosdetailsComponent},
-        {path: "carros/edit/:id", component: CarroslistComponent}
+        {path: "carros/edit/:id", component: CarroslistComponent},
+        {path: "marcas", component: MarcaslistComponent},
+        {path: "marcas/new", component: MarcasdetailsComponent},
+        {path: "marcas/edit/:id", component: MarcasdetailsComponent}
     ]}
 ];
